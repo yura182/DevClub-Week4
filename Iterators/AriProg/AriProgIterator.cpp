ed38ce8d943350d91a9f10f7239673ed19e2f97a
yura182 <yuriy.petrahenko7@gmail.com>
2019-04-16 20:36:56 +0300

    [Iterators] Add header

--- Iterators/AriProg/AriProgIterator.cpp
+++ Iterators/AriProg/AriProgIterator.cpp
@@ -1,1 +1,87 @@
+#include "AriProgIterator.h"
 
+AriProgIterator::AriProgIterator(int first, int step, int limit) {
+    this->first = first;
+    this->step = step;
+    this->limit = limit;
+    this->currentValue = first;
+    this->currentIndex = 0;
+}
+
+AriProgIterator::~AriProgIterator() {}
+
+int AriProgIterator::value() const {
+    return this->currentValue;
+}
+
+bool AriProgIterator::end() const {
+    return this->currentValue > this->limit;
+}
+
+bool AriProgIterator::begin() const {
+    return this->currentValue < this->first;
+}
+
+void AriProgIterator::next() {
+    if ( this->end() ) {
+        return;
+    }
+    this->currentIndex += 1;
+    this->currentValue += this->step;
+}
+
+void AriProgIterator::prev() {
+    if ( this->begin() ) {
+        return;
+    }
+    this->currentIndex -= 1;
+    this->currentValue -= this->step;
+}
+
+void AriProgIterator::move(int index) {
+    int temp = this->currentIndex;
+    
+    this->currentIndex = index - 1;
+    this->currentValue = this->currentIndex * this->step + this->first;
+    
+    if ( this->begin() || this->end() ) {
+        this->currentIndex = temp;
+        this->currentValue = this->currentIndex * this->step + this->first;
+        return;
+    }
+}
+
+void AriProgIterator::reset() {
+    this->currentIndex = 0;
+    this->currentValue = this->first;
+}
+
+void AriProgIterator::operator++() {
+    this->next();
+}
+
+void AriProgIterator::operator++(int) {
+    this->next();
+}
+
+void AriProgIterator::operator--() {
+    this->prev();
+}
+
+void AriProgIterator::operator--(int) {
+    this->prev();
+}
+
+int AriProgIterator::operator*() const {
+    return this->value();
+}
+
+int AriProgIterator::operator[](int index) const {
+    int result = this->step * index + this->first;
+    
+    if ( result > this->limit || result < this->first ) {
+        return;
+    }
+    
+    return result;
+}
--- Iterators/AriProg/AriProgIterator.h
+++ Iterators/AriProg/AriProgIterator.h
@@ -1,6 +1,31 @@
 #ifndef ARIPROGITERATOR_H
 #define ARIPROGITERATOR_H
 
-
+class AriProgIterator {
+    private:
+        int first;
+        int step;
+        int last;
+        int currentValue;
+        int currentIndex;
+    public:
+        AriProgIterator(int first, int step, int last);
+        ~AriProgIterator();
+        
+        int value() const;
+        bool end() const;
+        bool begin() const;
+        void next();
+        void prev();
+        void move(int index);
+        void reset();
+        
+        void operator++();
+        void operator++(int);
+        void operator--();
+        void operator--(int);
+        int operator*() const;
+        int operator[](int index) const;
+};
 
 #endif // AROPROGITERATOR_H
