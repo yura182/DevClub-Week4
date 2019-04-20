2a00f42bd721651bd44ff8d9c4a53ee738202ceb
yura182 <yuriy.petrahenko7@gmail.com>
2019-04-20 17:56:29 +0300

    [fibonacci] add h

--- Iterators/Fibonacci/FibonacciIterator.h
+++ Iterators/Fibonacci/FibonacciIterator.h
@@ -1,6 +1,32 @@
 #ifndef FIBONACCIITERATOR_H
 #define FIBONACCIITERATOR_H
 
-
+class FibonacciIterator {
+    private:
+        int size;
+        int firstIndex;
+        int currentIndex;
+        int currentValue;
+        int prevValue;
+        int fibonacci(int number);
+    public:
+        FibonacciIterator(int size, int firstIndex);
+        ~FibonacciIterator();
+        
+        int value() const;
+        bool start() const;
+        bool end() const;
+        void next();
+        void prev();
+        void move(int index);
+        void reset();
+        
+        void operator++();
+        void operator++(int);
+        void operator--();
+        void operator--(int);
+        int operator*();
+        int operator[]();
+};
 
 #endif // FIBONACCIITERATOR_H
