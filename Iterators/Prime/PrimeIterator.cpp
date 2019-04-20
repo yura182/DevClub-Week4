8dd8a22543d4c426a1324525a3dfb3f09f7d1670
yura182 <yuriy.petrahenko7@gmail.com>
2019-04-20 19:48:05 +0300

    [prime] add constructor

--- Iterators/Prime/PrimeIterator.cpp
+++ Iterators/Prime/PrimeIterator.cpp
@@ -1,1 +1,17 @@
-PrimeIterator
+#include <cstdlib>
+#include "PrimeIterator.h"
+
+PrimeIterator::PrimeIterator(int size) {
+    this->size = size;
+    this->currentIndex = 0;
+    this->initSize = 5;
+    this->primeNumbers = (*int) calloc(this->size, sizeof(int));
+    
+    if ( this->primeNumbers == NULL ) {
+        throw OutOfMemoryException();
+    }
+    
+    if ( this->size < this->initSize ) {
+        this->initSize = this->size;
+    }
+}
--- Iterators/Prime/PrimeIterator.h
+++ Iterators/Prime/PrimeIterator.h
@@ -1,12 +1,16 @@
 #ifndef PRIMEITERATOR_H
 #define PRIMEITERATOR_H
 
+class OutOfMemoryException {};
+
 class PrimeIterator {
     private:
         int size;
+        int initSize;
         int currentIndex;
         int *primeNumbers;
         bool isPrime(int n);
+        void fillArray(int *array, int start, int end, int size);
     public:
         PrimeIterator(int size=10);
         ~PrimeIterator();
