fb9536630fc5e0d4c9175744084116d11f64a28f
yura182 <yuriy.petrahenko7@gmail.com>
2019-04-25 15:31:43 +0300

    [statistic] finish statistic

--- statistic/TextHandler.cpp
+++ statistic/TextHandler.cpp
@@ -7,6 +7,8 @@ TextHandler::TextHandler(const char* filename) : filename(filename) {
     numbersStatistics = new std::map<char, int>();
     specialSymbols = new std::set<char>();
     specialSymbolsStatistics = new std::map<char, int>();
+    words = new std::set<std::string>();
+    wordsStatistics = new std::map<std::string, int>();
     quantity = 0;
 }
 
@@ -17,26 +19,42 @@ TextHandler::~TextHandler() {
     delete numbersStatistics;
     delete specialSymbols;
     delete specialSymbolsStatistics;
+    delete words;
+    delete wordsStatistics;
 }
 
 const std::set<char>& TextHandler::getCharacters() const {
     return *characters;
 }
+
 const std::map<char, int>& TextHandler::getCharactersStatistics() const {
     return *charactersStatistics;
 }
+
 const std::set<char>& TextHandler::getNumbers() const {
     return *numbers;
 }
+
 const std::map<char, int>& TextHandler::getNumbersStatistics() const {
     return *numbersStatistics;
 }
+
 const std::set<char>& TextHandler::getSpecialSymbols() const {
     return *specialSymbols;
 }
+
 const std::map<char, int>& TextHandler::getSpecialSymbolsStatistics() const {
     return *specialSymbolsStatistics;
 }
+
+const std::set<std::string>& TextHandler::getWords() const {
+    return *words;
+}
+
+const std::map<std::string, int>& TextHandler::getWordsStatistics() const {
+    return *wordsStatistics;
+}
+
 long long TextHandler::getQuantity() const {
     return quantity;
 }
@@ -52,12 +70,25 @@ void TextHandler::insert(char symbol, std::map<char, int>* lst) {
     lst->at(symbol) += 1;
 }
 
+void TextHandler::insert(std::string word, std::set<std::string>* lst) {
+    lst->insert(word);
+}
+
+void TextHandler::insert(std::string word, std::map<std::string, int>* lst) {
+    if ( lst->find(word) == lst->end() ) {
+        lst->insert(std::pair<std::string, int>(word, 0));
+    }
+    lst->at(word) += 1;
+}
+
 bool TextHandler::isLetter(char symbol) {
     return ( symbol >= 'A' && symbol <= 'Z' ) || ( symbol >= 'a' && symbol <= 'z' );
 }
+
 bool TextHandler::isNumber(char symbol) {
     return symbol >= '0' && symbol <= '9';
 }
+
 bool TextHandler::isSpecial(char symbol) {
     bool special = false;
 
@@ -70,13 +101,39 @@ bool TextHandler::isSpecial(char symbol) {
     return special;
 }
 
+bool TextHandler::isEndOfWord(char symbol) {
+    if ( symbol == ' ' ) {
+        return true;
+    }
+    
+    if ( isSpecial(symbol) ) {
+        if ( isSpecialNotEnd(symbol) ) {
+            return false;
+        }
+        return true;
+    }
+    return false;
+}
+
+bool TextHandler::isSpecialNotEnd(char symbol) {
+    if ( symbol == '\'' ) {
+        return true;
+    }
+    if ( symbol == '-' ) {
+        return true;
+    }
+    return false;
+}
+
 void TextHandler::parseText() {
     std::ifstream file(filename);
     int diff = 'a' - 'A';
     char symbol;
+    std::string word;
 
     for ( ; file.get(symbol) ; ) {
         if ( isLetter(symbol) ) {
+            word += symbol;
             if ( symbol < 'a' ) {
                 symbol += diff;
             }
@@ -93,6 +150,14 @@ void TextHandler::parseText() {
             insert(symbol, specialSymbols);
             insert(symbol, specialSymbolsStatistics);
             quantity += 1;
+        }
+        if ( isSpecialNotEnd(symbol) ) {
+            word += symbol;
+        }
+        if ( isEndOfWord(symbol) && !word.empty() ) {
+            insert(word, words);
+            insert(word, wordsStatistics);
+            word.clear();
         }
     }
     file.close();
@@ -114,5 +179,10 @@ std::ostream& operator<<(std::ostream& out, const TextHandler& handler) {
     out << "Unique: " << handler.getSpecialSymbols() << std::endl;
     out << "Matches in text: " << std::endl;
     out << handler.getSpecialSymbolsStatistics() << std::endl;
+    
+    out << "Words statistic" << std::endl;
+    out << "Unique: " << handler.getWords() << std::endl;
+    out << "Matches in text: " << std::endl;
+    out << handler.getWordsStatistics() << std::endl;
     return out;
 }
--- statistic/TextHandler.h
+++ statistic/TextHandler.h
@@ -16,13 +16,19 @@ class TextHandler {
         std::map<char, int>* numbersStatistics;
         std::set<char>* specialSymbols;
         std::map<char, int>* specialSymbolsStatistics;
+        std::set<std::string>* words;
+        std::map<std::string, int>* wordsStatistics;
         long long quantity;
 
         void insert(char symbol, std::set<char>* lst);
         void insert(char symbol, std::map<char, int>* lst);
+        void insert(std::string word, std::set<std::string>* lst);
+        void insert(std::string word, std::map<std::string, int>* lst);
         bool isLetter(char symbol);
         bool isNumber(char symbol);
         bool isSpecial(char symbol);
+        bool isEndOfWord(char symbol);
+        bool isSpecialNotEnd(char symbol);
     public:
         TextHandler(const char* filename);
         ~TextHandler();
@@ -33,6 +39,8 @@ class TextHandler {
         const std::map<char, int>& getNumbersStatistics() const;
         const std::set<char>& getSpecialSymbols() const;
         const std::map<char, int>& getSpecialSymbolsStatistics() const;
+        const std::set<std::string>& getWords() const;
+        const std::map<std::string, int>& getWordsStatistics() const;
         long long getQuantity() const;
 
         void parseText();
