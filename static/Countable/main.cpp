8c698631a8348d5cea12025a790a6c26439cd762
yura182 <yuriy.petrahenko7@gmail.com>
2019-04-26 17:54:29 +0300

    [countable] change main

--- static/Countable/main.cpp
+++ static/Countable/main.cpp
@@ -2,12 +2,18 @@
 #include "Countable.h"
 
 int main() {
-    Countable a;
-    Countable b;
-    Countable c;
-    //Countable d;
+    Countable *a = new Countable();
+    Countable *b = new Countable();
+    Countable *c = new Countable();
+    Countable *d = new Countable();
+    
+    delete(a);
     
     std::cout << Countable::getTotal() << std::endl;
+    
+    delete(b);
+    delete(c);
+    delete(d);
     
     return 0;
 }
